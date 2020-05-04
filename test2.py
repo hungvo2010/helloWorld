b7c1b47588ed1785974dd5ba8a5bbfa1f00c7d62
hungvo2010 <62180150+hungvo2010@users.noreply.github.com>
2020-05-04 15:12:56 +0700

    Sach giao khoa lap trinh Maximum Subarray

--- test2.py
+++ test2.py
@@ -1,15 +1,30 @@
+import math
 def maximumSum(a, m):
-    n=len(a)
-    arr=[0 for i in range(m)]
-    for i in a:
-        arr[i%m]+=1
-    sumx=-math.inf
-    maxx=-math.inf
-    for i in range(m):
-            if sumx>0:
-                sumx+=arr[i]
-            else:
-                sumx=arr[i]
-            if sumx>maxx and sumx<m:
-                maxx=sumx
-    return maxx 
+    n = len(a)
+    if n == 0:
+        return 0
+    # build the potential phi in [0, m-1]
+    phi = [(a[0] % m, 0)]
+    # current max sum
+    max_sum = phi[0][0]
+    for i in range(1, n):
+        pot = (phi[-1][0] + a[i]) % m
+        phi.append((pot, i))
+        if pot > max_sum:
+             max_sum = pot
+ 
+    # we now need to look for negative cases
+    phi.sort()
+    candidate=0
+    for j in range(n-1, 0, -1):
+        pot, pos = phi[j]
+        pot_prev, pos_prev = phi[j-1]
+        # we must have pos_prev > pos
+        if pos_prev > pos:
+            candidate = (pot_prev - pot) % m
+        if candidate > max_sum:
+            max_sum = candidate
+    return max_sum
+a=[int(x) for x in input().split()]
+m=int(input())
+print(maximumSum(a,m))
